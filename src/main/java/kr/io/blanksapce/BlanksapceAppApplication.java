dfa28d9c5e2502eda3e12b37ce6b6066be2c6b86
blank0914 <yerin0373@gmail.com>
2025-12-27 21:09:29 +0900

    Initial commit

---
+++ src/main/java/kr/io/blanksapce/BlanksapceAppApplication.java
@@ -0,0 +1,13 @@
+package kr.io.blanksapce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlanksapceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlanksapceAppApplication.class, args);
+	}
+
+}
