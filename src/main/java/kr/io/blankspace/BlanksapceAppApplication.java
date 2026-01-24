10c51b3368199ad4719ef6cc21161edebe30067c
blank0914 <yerin0373@gmail.com>
2026-01-24 17:36:48 +0900

    fix: 오래된 로그인 기록이 자동 삭제되지 않는 문제 해결

--- src/main/java/kr/io/blankspace/BlanksapceAppApplication.java
+++ src/main/java/kr/io/blankspace/BlanksapceAppApplication.java
@@ -2,9 +2,11 @@ package kr.io.blankspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class BlanksapceAppApplication
-{ public static void main(String[] args) {
-		SpringApplication.run(BlanksapceAppApplication.class, args);
-	} }
+public class BlanksapceAppApplication {
+	public static void main(String[] args)
+	{ SpringApplication.run(BlanksapceAppApplication.class, args); }
+}
